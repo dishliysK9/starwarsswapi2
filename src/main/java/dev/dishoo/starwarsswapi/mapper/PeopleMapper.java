7b1a5b92ef635ab0e52d8e828992470a8a83f493
DishliysK9 <201221013@fdiba.tu-sofia.bg>
2023-05-12 18:53:00 +0300

    feat: repos, service layer, validation, errors and others added

---
+++ src/main/java/dev/dishoo/starwarsswapi/mapper/PeopleMapper.java
@@ -0,0 +1,8 @@
+package dev.dishoo.starwarsswapi.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface PeopleMapper {
+    
+}
---
+++ src/main/java/dev/dishoo/starwarsswapi/web/PlanetsController.java
@@ -0,0 +1,13 @@
+package dev.dishoo.starwarsswapi.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/planets")
+@AllArgsConstructor
+public class PlanetsController {
+    
+}
