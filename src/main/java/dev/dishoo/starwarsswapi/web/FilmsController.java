8735164f290eeacad6c38ee96e37e521f72735d6
DishliysK9 <201221013@fdiba.tu-sofia.bg>
2023-05-12 19:14:25 +0300

    test commit

--- src/main/java/dev/dishoo/starwarsswapi/web/FilmsController.java
+++ src/main/java/dev/dishoo/starwarsswapi/web/FilmsController.java
@@ -3,6 +3,7 @@ package dev.dishoo.starwarsswapi.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.dishoo.starwarsswapi.service.FilmsService;
 import dev.dishoo.starwarsswapi.validation.ObjectValidator;
 import lombok.AllArgsConstructor;
 
@@ -12,5 +13,10 @@ import lombok.AllArgsConstructor;
 public class FilmsController {
     
     private ObjectValidator validator;
+
+
+
+    private FilmsService filmsService;
+
 
 }
